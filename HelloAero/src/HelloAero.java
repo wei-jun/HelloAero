9f7592bdc8937434155ac70cbd0cf837586d6d81
Wei-Jun <weijun.zeng@mail.utoronto.ca>
2018-05-21 18:23:46 -0400

    Hello changed

--- HelloAero/src/HelloAero.java
+++ HelloAero/src/HelloAero.java
@@ -20,13 +20,13 @@ import javax.xml.parsers.ParserConfigurationException;
 
 import org.xml.sax.SAXException;
 
-
+ 
 public class HelloAero {
 	static Icon logo;
 	private static final String PROPERTY_PATH = "property_path";
 
 	public HelloAero() throws ParserConfigurationException, SAXException, IOException {		
-		System.out.println("Hello World, and Hello AeroSoft!");
+		System.out.println("Hello World, Hello AeroSoft!");
 //		ImageIcon icon = createImageIcon("/images/aerosoftsys.gif", "company icon");
 //		JFrame frame new LogoFrame(icon);
 		
