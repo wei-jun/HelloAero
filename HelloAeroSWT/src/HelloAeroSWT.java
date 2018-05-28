48355a88075266e02cca259d3e88098caae7ced4
Wei-Jun <weijun.zeng@mail.utoronto.ca>
2018-05-27 23:06:50 -0400

    add widgets in shell

--- HelloAeroSWT/src/HelloAeroSWT.java
+++ HelloAeroSWT/src/HelloAeroSWT.java
@@ -1,7 +1,15 @@
 /* 2018-05-19 */
 
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
 
 
 public class HelloAeroSWT {
@@ -10,11 +18,36 @@ public class HelloAeroSWT {
 		// TODO Auto-generated method stub	
 		Display display = new Display();
 		Shell shell = new Shell(display);
-		shell.setText("Hello AeroSoft!");
-		shell.open();
+		shell.setText("Hello World!, Hello AeroSoft");		
+		shell.setLayout(new FillLayout());
+		
+		Label label = new Label(shell, SWT.BORDER);
+        label.setText("This is a label:");
+        
+        Text text = new Text(shell, SWT.NONE);
+        text.setText("This is the text in the text widget");
+		
+		Button button =  new Button(shell, SWT.PUSH);
+		button.setText("push button");
+		//register listener for the selection event
+		button.addSelectionListener(new SelectionAdapter() {
+		    @Override
+		    public void widgetSelected(SelectionEvent e) {
+		        System.out.println("Called!");
+		    }
+		});
+		
+		shell.open();		
+		// run the event loop as long as the window is open		
 		while (!shell.isDisposed()) {
-		if (!display.readAndDispatch()) display.sleep();
+			// read the next OS event queue and transfer it to a SWT event
+			if (!display.readAndDispatch()) {
+				// if there are currently no other OS event to process
+			    // sleep until the next OS event is available
+				display.sleep();
+			}			
 		}
+		// disposes all associated windows and their components
 		display.dispose();
 	}
 
